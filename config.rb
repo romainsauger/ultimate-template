8e2c3f58b78fa497c54cd797948d50b8469b9fe1
romainsauger <romain.sauger@gmail.com>
2015-04-03 13:50:12 +0200

    test

---
+++ config.rb
@@ -0,0 +1,10 @@
+require 'breakpoint' 
+require "normalize-scss"
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
